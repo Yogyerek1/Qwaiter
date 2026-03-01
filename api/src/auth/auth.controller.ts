135a6f49eace5eb852ce2331f45968572239e8bb
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-01 17:36:37 +0100

    feat(QW-02): add JwtModule and JwtStrategy to auth.module.ts

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -29,6 +29,6 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @Post('me')
   getMe(@Request() req: any) {
-    return req.user;
+    return req.user; // JwtStrategy validate()
   }
 }
--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -4,10 +4,20 @@ import { User } from '../entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
