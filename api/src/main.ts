cf9638a5de68033ac591a8b3090fcb3344d73a42
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-09 12:15:29 +0100

    chore(main): configure swagger in main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Qwaiter API')
+    .setDescription('Qwaiter backend API documentation')
+    .setVersion('0.1')
+    .addCookieAuth('access_token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   if (process.env.NODE_ENV === 'production') {
     app.getHttpAdapter().getInstance().set('trust proxy', 1);
