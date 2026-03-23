6eb7512d30393d6ee0852ca67cacdee03aa318fd
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-23 16:42:22 +0100

    feat(QW-04): add GuestGuard for handling guest authentication

--- api/src/guest/guest.module.ts
+++ api/src/guest/guest.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { GuestController } from './guest.controller';
 import { GuestService } from './guest.service';
+import { GuestGuard } from './guest.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [GuestController],
-  providers: [GuestService],
+  providers: [GuestService, GuestGuard],
+  exports: [GuestService],
 })
 export class GuestModule {}
--- api/src/guest/guest.service.ts
+++ api/src/guest/guest.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class GuestService {}
+export class GuestService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  generateGuestToken() {
+    const payload = {
+      sub: randomUUID(),
+      type: 'guest',
+    };
+
+    return this.jwtService.sign(payload);
+  }
+}
