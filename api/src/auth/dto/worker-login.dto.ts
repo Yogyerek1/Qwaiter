403f146958cd2987ebb6d0b4edb8232174b4aa8a
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-28 16:39:55 +0100

    feat(QW-04): add ApiProperty and validators to WorkerLoginDto

--- api/src/auth/dto/worker-login.dto.ts
+++ api/src/auth/dto/worker-login.dto.ts
@@ -1,4 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
 export class WorkerLoginDto {
+  @ApiProperty({ example: '' })
+  @IsString()
   username: string;
+
+  @ApiProperty({ example: '' })
+  @IsString()
   password: string;
 }
