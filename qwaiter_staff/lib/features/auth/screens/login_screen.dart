ac3aa975a3b17449549dd30334363eba3757ab73
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-28 17:28:22 +0100

    feat(QW-05): create LoginScreen

---
+++ qwaiter_staff/lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../auth_provider.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
