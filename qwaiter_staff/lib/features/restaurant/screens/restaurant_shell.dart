17031d0dc4eb91aa441e3518a7ebff089c4c03f0
D0M1N1K_. <milkovicsd32@gmail.com>
2026-03-29 13:38:32 +0200

    feat(QW-05): create screens and convert RestaurantShell to stateful

--- qwaiter_staff/lib/features/restaurant/screens/restaurant_shell.dart
+++ qwaiter_staff/lib/features/restaurant/screens/restaurant_shell.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../screens/workers_screen.dart';
+import '../screens/menu_screen.dart';
+import '../screens/tables_screen.dart';
+import '../screens/settings_screen.dart';
 
-class RestaurantShell extends StatelessWidget {
+class RestaurantShell extends StatefulWidget {
   final String restaurantId;
   const RestaurantShell({super.key, required this.restaurantId});
 
   @override
+  State<RestaurantShell> createState() => _RestaurantShellState();
+}
+
+class _RestaurantShellState extends State<RestaurantShell> {
+  int _currentIndex = 0;
+
+  late final List<Widget> _screens;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Restaurant $restaurantId')),
+      appBar: AppBar(title: Text('Restaurant ${widget.restaurantId}')),
       body: const Center(child: Text('Hamarosan')),
     );
   }
---
+++ qwaiter_staff/lib/features/restaurant/screens/workers_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorkersScreen extends StatefulWidget {
+  const WorkersScreen({Key? key}) : super(key: key);
+
+  @override
+  _WorkersScreenState createState() => _WorkersScreenState();
+}
+
+class _WorkersScreenState extends State<WorkersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
