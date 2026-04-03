ec72e0dd8899a77343606d5929bdd6093c92a9fd
D0M1N1K_. <milkovicsd32@gmail.com>
2026-04-03 18:10:05 +0200

    refactor(QW-05): convert MenuScreen from stateless to stateful widget

--- qwaiter_staff/lib/features/restaurant/screens/menu_screen.dart
+++ qwaiter_staff/lib/features/restaurant/screens/menu_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MenuScreen extends StatelessWidget {
+class MenuScreen extends StatefulWidget {
   final String restaurantId;
   const MenuScreen({super.key, required this.restaurantId});
 
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return const Center(child: Text('Menu - hamarosan'));
