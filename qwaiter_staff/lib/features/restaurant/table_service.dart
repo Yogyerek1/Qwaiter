737aa70a8174c1e1419529ccc9602b7d3fc4eb3a
D0M1N1K_. <milkovicsd32@gmail.com>
2026-04-03 15:29:13 +0200

    feat(QW-05): add getTables to TableService

--- qwaiter_staff/lib/features/restaurant/table_service.dart
+++ qwaiter_staff/lib/features/restaurant/table_service.dart
@@ -3,4 +3,13 @@ import 'package:qwaiter_staff/core/network/dio_client.dart';
 
 class TableService {
   final Dio _dio = DioClient().dio;
+
+  Future<List<dynamic>> getTables(String restaurantID) async {
+    try {
+      final response = await _dio.get('/user/tables/$restaurantID');
+      return response.data as List<dynamic>;
+    } on DioException catch (e) {
+      throw e.response?.data['message'] ?? 'Failed to fetch tables';
+    }
+  }
 }
