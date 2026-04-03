0e21989566128e3e0a84a1f54e8382d02c8f4e89
D0M1N1K_. <milkovicsd32@gmail.com>
2026-04-03 15:58:49 +0200

    refactor(QW-05): convert TablesScreen from Stateless to Stateful widget

--- qwaiter_staff/lib/features/restaurant/screens/tables_screen.dart
+++ qwaiter_staff/lib/features/restaurant/screens/tables_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-class TablesScreen extends StatelessWidget {
+class TablesScreen extends StatefulWidget {
   final String restaurantId;
   const TablesScreen({super.key, required this.restaurantId});
 
+  @override
+  State<TablesScreen> createState() => _TablesScreenState();
+}
+
+class _TablesScreenState extends State<TablesScreen> {
   @override
   Widget build(BuildContext context) {
     return const Center(child: Text('Tables - hamarosan'));
