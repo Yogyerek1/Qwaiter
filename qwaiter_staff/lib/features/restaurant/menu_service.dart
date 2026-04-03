76229e48a321f7b7826d9c5d54ee7b5715ba76a7
D0M1N1K_. <milkovicsd32@gmail.com>
2026-04-03 17:02:56 +0200

    feat(QW-05): add MenuService and add getCategories

---
+++ qwaiter_staff/lib/features/restaurant/menu_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:qwaiter_staff/core/network/dio_client.dart';
+
+class MenuService {
+  final Dio _dio = DioClient().dio;
+
+  Future<List<dynamic>> getCategories(String restaurantID) async {
+    try {
+      final response = await _dio.get('/user/categories/$restaurantID');
+      return response.data as List<dynamic>;
+    } on DioException catch (e) {
+      throw e.response?.data['message'] ?? 'Failed to get categories';
+    }
+  }
+}
