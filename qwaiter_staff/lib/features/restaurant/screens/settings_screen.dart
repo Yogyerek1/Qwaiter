5b3749caa4e59c520a4d981592e842920159a01c
D0M1N1K_. <milkovicsd32@gmail.com>
2026-04-25 10:52:15 +0200

    refactor(QW-05): delete settings provider and service

--- qwaiter_staff/lib/features/restaurant/screens/settings_screen.dart
+++ qwaiter_staff/lib/features/restaurant/screens/settings_screen.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qwaiter_staff/features/restaurant/providers/restaurant_provider.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   final String restaurantId;
   const SettingsScreen({super.key, required this.restaurantId});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Settings - hamarosan'));
+    final provider = context.watch<RestaurantProvider>();
+    final restaurant = provider.restaurants.firstWhere(
+      (r) => r.id == widget.restaurantId,
+    );
+
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Name: ${restaurant.name}'),
+            Text('Address: ${restaurant.address}'),
+          ],
+        ),
+      ),
+    );
   }
 }
